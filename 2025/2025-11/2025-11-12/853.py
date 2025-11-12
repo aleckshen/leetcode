4b36b8807762cb9ef7674ae260ab9bfcc885d4ff
aleckshen <aleckshn@gmail.com>
2025-11-12 21:15:58 +1300

    853

---
+++ 2025/2025-11/2025-11-12/853.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        pairs = [(p, s) for p, s in zip(position, speed)]
+        pairs.sort(reverse=True)
+        stack = []
+
+        for p, s in pairs:
+            stack.append((target - p) / float(s))
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
