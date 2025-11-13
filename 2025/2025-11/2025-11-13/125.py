d7d19332111c08d213dbbd28a62090cd953b60f8
aleckshen <aleckshn@gmail.com>
2025-11-13 17:02:15 +1300

    125

---
+++ 2025/2025-11/2025-11-13/125.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+    def alphaNum(self, ch):
+        return (ord('A') <= ord(ch) <= ord('Z') or
+                ord('a') <= ord(ch) <= ord('z') or
+                ord('0') <= ord(ch) <= ord('9'))
