731f0aa0d4120eedecb6d3b06f12e07c267b8900
aleckshen <aleckshn@gmail.com>
2025-11-25 11:05:31 +1300

    235

---
+++ 2025/2025-11/2025-11-25/235.py
@@ -0,0 +1,17 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        current = root
+
+        while current:
+            if p.val < current.val and q.val < current.val:
+                current = current.left
+            elif p.val > current.val and q.val > current.val:
+                current = current.right
+            else:
+                return current
