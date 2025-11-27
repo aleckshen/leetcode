738a1e2934998f278549d4943647de4247ad9d16
aleckshen <aleckshn@gmail.com>
2025-11-28 00:37:22 +1300

    973

---
+++ 2025/2025-11/2025-11-27/973.py
@@ -0,0 +1,17 @@
+import heapq, math
+
+class Solution(object):
+    def kClosest(self, points, k):
+        maxHeap = []
+        res = []
+
+        for point in points:
+            distance = math.sqrt((point[0] * point[0]) + (point[1] * point[1]))
+
+        while len(maxHeap) > k:
+            heapq.heappop(maxHeap)
+
+        for tup in maxHeap:
+            res.append(tup[1])
+
+        return res
